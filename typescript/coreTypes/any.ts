8cf3b02e811cf7ae36b017b5e4da1dc36e971794
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-12-10 09:32:49 -0300

    add any challenge

---
+++ typescript/coreTypes/any.ts
@@ -0,0 +1,12 @@
+/* Challenge
+
+Declare a variable named dynamicData with the type any and assign it the number 42.
+
+Then reassign dynamicData to the string "Hello World" to demonstrate the flexibility of the any type.
+
+Finally, print the variable to the console to show its current value. */
+
+// TODO: Write your code here
+let dynamicData: any = 42;
+dynamicData = 'Hello World'
+console.log(dynamicData)
