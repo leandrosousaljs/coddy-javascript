39915bdaae26acae4c4d25801fc4d6d20ddc5edb
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-12-14 07:57:28 -0300

    add explicit type challenge

---
+++ typescript/coreTypes/explicitType.ts
@@ -0,0 +1,22 @@
+/* Challenge
+
+Declare a variable named totalScore with the explicit type number without assigning it an initial value.
+
+On the next line, assign the value 95 to the totalScore variable.
+
+Then declare another variable named playerName with the explicit type string without assigning it an initial value.
+
+On the next line, assign the value "Alex" to the playerName variable.
+
+Finally, print both variables to the console on separate lines, first totalScore then playerName. */
+
+// TODO: Write your code here
+
+let totalScore: number;
+let playerName: string;
+
+totalScore = 95;
+playerName = 'Alex';
+
+console.log(totalScore);
+console.log(playerName);
