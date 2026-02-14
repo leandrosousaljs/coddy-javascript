8cb678b386ba7b7c91ca1975d736ef1c6bc1e97b
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-02-14 14:00:07 -0300

    add object oriented programming section

---
+++ OOP/reviewObj.ts
@@ -0,0 +1,13 @@
+/* Challenge
+
+Print the player's level in the console. */
+
+const player = {
+  name: 'Alex',
+  health: 100,
+  level: 2,
+  inventory: ['sword', 'potion'],
+};
+
+// TODO: print the player's level in the console
+console.log(player.level);
