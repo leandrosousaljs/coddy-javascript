d5a391f688e87d41fe799fc27a26892fca99b8c0
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2026-03-17 08:17:05 -0300

    add static properties challenge

---
+++ OOP/staticMethods&Properties/staticProperties/constants.ts
@@ -0,0 +1,6 @@
+export class MathConstants {
+  // TODO: Add a static property called PI and set its value to 3.14159
+  static PI: number = 3.14159;
+  // TODO: Add a static property called E and set its value to 2.71828
+  static E: number = 2.71828;
+}
---
+++ OOP/staticMethods&Properties/staticProperties/driver.ts
@@ -0,0 +1,5 @@
+import { MathConstants } from './constants.ts';
+
+// Test code - don't modify
+console.log(MathConstants.PI); // Should output 3.14159
+console.log(MathConstants.E); // Should output 2.71828
